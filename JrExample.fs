2a290e82525c258c797b31676c0d3b03be7c652a
y2k <itwisterlx@gmail.com>
2018-11-21 11:14:17 +0300

    format code

--- JrExample.fs
+++ JrExample.fs
@@ -22,25 +22,25 @@ type FaceProgram<'a> =
 module Free =
     let rec bind f =
         let mapI f =
-            function 
+            function
             | HtmlRequest(x, next) -> HtmlRequest(x, next >> f)
             | ApiRequest(x, next) -> ApiRequest(x, next >> f)
-        function 
+        function
         | Free x -> Free(mapI (bind f) x)
         | Pure x -> f x
-    
+
     type FreeBuilder() =
         member __.Bind(x, f) = bind f x
         member __.Return x = Pure x
         member __.ReturnFrom x = x
         member __.Zero() = Pure()
-    
+
     let free = FreeBuilder()
 
 let apiRequest (f: Replay<'x> -> ApiRequest): FaceProgram<'x> = failwith "TODO"
 
 let requestWithParse siteUrl f =
-    Free.free { 
+    Free.free {
         let uri = Uri siteUrl
         let! html = Free(HtmlRequest(uri, Pure))
         let! response = apiRequest (fun x -> f (html, x))
@@ -49,9 +49,10 @@ let requestWithParse siteUrl f =
     }
 
 let requestWithParse' _ f =
-    Free.free { 
-        let fr = f ("", fun _-> ())
-        let url = 
+    Free.free {
+        let fr = f ("", fun _ -> ())
+
+        let url =
             match fr with
             | TagListRequest _ -> "TODO"
             | _ -> "TODO"
@@ -59,12 +60,10 @@ let requestWithParse' _ f =
     }
 
 let test =
-    Free.free { 
+    Free.free {
         let! x1 = requestWithParse "http://joyreactor.cc/user/_y2k" ProfileRequest
         printfn "Profile %O %O" x1.userImage x1.rating
-
         let! x2 = requestWithParse "http://joyreactor.cc/" TagListRequest
         printfn "Tags %O" x2.Length
-
         return ()
     }
